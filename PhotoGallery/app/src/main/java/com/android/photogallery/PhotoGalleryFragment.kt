83bacb5a5eae8ee35b33e2fd1d1703933a3fd86b
Aishwarya Mallampati <aishwarya.mallampati@gmail.com>
2020-06-24 23:16:27 -0400

    Added FlickrFetchr to fetch Flickr home page data

---
+++ PhotoGallery/app/src/main/java/com/android/photogallery/PhotoGalleryFragment.kt
@@ -0,0 +1,47 @@
+package com.android.photogallery
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.photogallery.repository.FlickrFetchr
+
+class PhotoGalleryFragment : Fragment() {
+    companion object {
+        private const val TAG = "PhotoGalleryFragment"
+        fun newInstance() = PhotoGalleryFragment()
+    }
+
+    private lateinit var rvPhoto: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i(TAG, "onCreate()")
+        super.onCreate(savedInstanceState)
+        val flickrLiveData: LiveData<String> = FlickrFetchr().fetchContents()
+        flickrLiveData.observe(
+            this,
+            Observer { responseString ->
+                Log.d(TAG, "Response recerived: $responseString")
+            }
+        )
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        Log.i(TAG, "onCreateView()")
+        val view = inflater.inflate(R.layout.fragment_photo_gallery, container, false)
+        rvPhoto = view.findViewById(R.id.rv_photo)
+        rvPhoto.layoutManager = GridLayoutManager(context, 3)
+        return view
+    }
+
+}
---
+++ PhotoGallery/app/src/main/java/com/android/photogallery/repository/FlickrFetchr.kt
@@ -0,0 +1,46 @@
+package com.android.photogallery.repository
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.android.photogallery.api.FlickrApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+class FlickrFetchr {
+    companion object {
+        private const val TAG = "FlickrFetchr"
+    }
+
+    private val flickrApi: FlickrApi
+
+    init {
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://www.flickr.com/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+        flickrApi = retrofit.create(FlickrApi::class.java)
+    }
+
+    fun fetchContents(): LiveData<String> {
+        Log.i(TAG, "fetchContents()")
+        val responseLiveData: MutableLiveData<String> = MutableLiveData()
+        val flickrRequest: Call<String> = flickrApi.fetchContents()
+
+        // enqueue executes the web request
+        flickrRequest.enqueue(object : Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(TAG, "Failed to fetch photos", t)
+            }
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                Log.d(TAG, "Response received: S{response.body()}")
+                responseLiveData.value = response.body()
+            }
+        })
+        return responseLiveData
+    }
+}
